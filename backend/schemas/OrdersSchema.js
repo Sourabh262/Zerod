52f0424aa3a4d3be2c040e6687e70e716880cce4
Sourabh262 <sourabhmishra9039@gmail.com>
2026-09-03 13:37:00 +0530

    Initial commit Zerodha Clone

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,10 @@
+const {Schema} = require("mongoose");
+
+const OrdersSchema = ({
+    name: String,
+    qty: Number,
+    price: Number,
+    mode: String,
+});
+
+module.exports = {OrdersSchema};
---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+const Orders = () => {
+  //  const [allOrders , setAllOrders] = useState([]);
+
+  //  useEffect(() =>{
+  //   axios.get("http://localhost:3002/allOrders").then((res) => {
+  //     setAllOrders(res.data);
+  //   });
+  //  },[]);
+
+  return (
+    <div className="orders">
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
+
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
